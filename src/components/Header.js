907dc08b6c04c39bb6f5e1c48b8779472b867ef8
Ashish Chauhan <ashishchauhan3301@gmail.com>
2022-09-25 22:40:52 +0530

    Change background image, Add text and text styles

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,17 @@
 import React from 'react'
 import { Navbar, Nav, Container } from 'react-bootstrap'
+import "./Home.css"
 
 export default function Header() {
     return <>
-        <Navbar>
+        <Navbar collapseOnSelect expand="lg" >
             <Container fluid>
                 <Navbar.Brand href="/">
-                    <span className="heading-text">Ecocops-The Home of Eco Warriors</span>
+                    <span className="heading-text">Ecocops</span>
                 </Navbar.Brand>
                 <Nav className="d-flex justify-content-end">
-                    <Nav.Link herf="#placeholder">Past Events</Nav.Link>
-                    <Nav.Link herf="#placeholder">Upcoming Events</Nav.Link>
+                    <Nav.Link herf="#placeholder"><span className="heading-text page-links">Past Events</span></Nav.Link>
+                    <Nav.Link herf="#placeholder"><span className="heading-text page-links">Upcoming Events</span></Nav.Link>
                 </Nav>
             </Container>
         </Navbar>
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,20 @@
 import React from 'react'
-import { Container, Row } from 'react-bootstrap'
+import { Container, Row, Button } from 'react-bootstrap'
 import Header from "./Header.js"
 import "./Home.css"
 
 export default function Home() {
     return <Container fluid className="home-root-section">
-        <Row >
-            <Header />
+        <Header />
+        <Row className="home-row-section">
+            <Container className="home-text-section">
+                <Row>
+                    Ecocops-The Home of Eco Warriors<br />
+                </Row>
+                <Row>
+                    {/* Small text describing eco club */}
+                </Row>
+            </Container>
         </Row>
     </Container>
 }
