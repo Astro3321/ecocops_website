4475fdd0ee99a1382e1b5d9f2c08cc8bf4c694dc
Ashish Chauhan <ashishchauhan3301@gmail.com>
2022-11-05 18:47:12 +0530

    Add Admin Login Form

--- src/components/Admin.js
+++ src/components/Admin.js
@@ -1,9 +1,28 @@
 import React from 'react'
+import { Card, Form, Button } from 'react-bootstrap'
 
 export default function Admin() {
     return (
-        <div>
-            
-        </div>
+        <>
+        <Card className="d-flex justify-text-center mx-auto mt-4" style={{maxWidth: "400px"}}>
+            <Card.Body>
+                <h2 className="text-center mb-4">Login</h2>
+
+                <Form>
+                    <Form.Group id="email">
+                        <Form.Label>Email</Form.Label>
+                        <Form.Control type="email" required></Form.Control>
+                    </Form.Group>
+
+                    <Form.Group id="password">
+                        <Form.Label>Password</Form.Label>
+                        <Form.Control type="password" required></Form.Control>
+                    </Form.Group>
+
+                    <Button className="w-100 mt-4" type="submit">Login</Button>
+                </Form>
+            </Card.Body>
+        </Card>
+        </>
     )
 }
