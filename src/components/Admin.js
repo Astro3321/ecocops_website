c8ea530c69992db8b299568bf91974cf9229595f
Ashish Chauhan <ashishchauhan3301@gmail.com>
2022-11-05 21:41:46 +0530

    Add Admin Login

--- src/components/Admin.js
+++ src/components/Admin.js
@@ -1,25 +1,52 @@
-import React from 'react'
-import { Card, Form, Button } from 'react-bootstrap'
+import React, { useRef, useState } from 'react';
+import { Card, Form, Button, Alert } from 'react-bootstrap';
+import { useNavigate } from 'react-router-dom';
+import { useAuth } from '../contexts/AuthContext';
 
 export default function Admin() {
+    const emailRef = useRef();
+    const passwordRef = useRef();
+
+    const { login } = useAuth();
+    const [error, setError] = useState();
+    const [loading, setLoading] = useState(false);
+
+    const navigate = useNavigate();
+
+    async function handleSubmit(event){
+        event.preventDefault();
+
+        try{
+            setError('');
+            setLoading(true);
+            await login(emailRef.current.value, passwordRef.current.value);
+            navigate('/');
+        } catch {
+            setError("Failed to Log In");
+        }
+
+        setLoading(false);
+    }
+
     return (
         <>
         <Card className="d-flex justify-text-center mx-auto mt-4" style={{maxWidth: "400px"}}>
             <Card.Body>
                 <h2 className="text-center mb-4">Login</h2>
+                {error && <Alert variant="danger">{error}</Alert>}
 
-                <Form>
+                <Form onSubmit={handleSubmit}>
                     <Form.Group id="email">
                         <Form.Label>Email</Form.Label>
-                        <Form.Control type="email" required></Form.Control>
+                        <Form.Control type="email" ref={emailRef} required></Form.Control>
                     </Form.Group>
 
                     <Form.Group id="password">
                         <Form.Label>Password</Form.Label>
-                        <Form.Control type="password" required></Form.Control>
+                        <Form.Control type="password" ref={passwordRef} required></Form.Control>
                     </Form.Group>
 
-                    <Button className="w-100 mt-4" type="submit">Login</Button>
+                    <Button className="w-100 mt-4" disabled={loading} type="submit">Login</Button>
                 </Form>
             </Card.Body>
         </Card>
